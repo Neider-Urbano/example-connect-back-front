ef19b35683d6efbc0c71cc77cce56fed53957424
Neider Urbano <jjuly8julianur@gmail.com>
2023-07-20 10:43:48 -0500

    uri mongopruebas

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-// const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority`;
-const uri =
-  "mongodb+srv://vercel-admin-user:9zuv13psZZZE0nZv@cluster0.yuiduwe.mongodb.net/conect-front-back?retryWrites=true&w=majority";
+// const uri =
+//   `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority` ||
+//   `mongodb://localhost:${process.env.PORT_COMPASS}/test`;
+const uri = `mongodb+srv://mongopruebas:rQVMjIOr1O6qZXdV@cluster0.yuiduwe.mongodb.net/conect-front-back?retryWrites=true&w=majority`;
 const connectDb = async () => {
   try {
     await mongoose.connect(uri);
