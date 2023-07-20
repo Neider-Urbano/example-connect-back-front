8febfe13197a54de2ab2ec8c6e6c257d82ca5f37
Neider Urbano <jjuly8julianur@gmail.com>
2023-07-20 10:38:15 -0500

    uri coneccion

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import "dotenv/config";
 
 // const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority`;
-const uri = process.env.MONGODB_URI;
+const uri =
+  "mongodb+srv://vercel-admin-user:9zuv13psZZZE0nZv@cluster0.yuiduwe.mongodb.net/conect-front-back?retryWrites=true&w=majority";
 const connectDb = async () => {
   try {
     await mongoose.connect(uri);
