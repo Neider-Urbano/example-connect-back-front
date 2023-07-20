a52351a26c25854542a386b8b18f5a0ad74b2210
Neider Urbano <jjuly8julianur@gmail.com>
2023-07-20 10:36:12 -0500

    variables de entorno

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const uri =
-  `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority` ||
-  `mongodb://localhost:${process.env.PORT_COMPASS}/test`;
+// const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority`;
+const uri = process.env.MONGODB_URI;
 const connectDb = async () => {
   try {
     await mongoose.connect(uri);
