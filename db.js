6ee0eceedc780a671b19eee6fd3689206fd793e5
Neider Urbano <jjuly8julianur@gmail.com>
2023-07-20 10:54:19 -0500

    actualizar constant uri

--- db.js
+++ db.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-
-// const uri =
-//   `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority` ||
-//   `mongodb://localhost:${process.env.PORT_COMPASS}/test`;
-const uri = `mongodb+srv://mongopruebas:rQVMjIOr1O6qZXdV@cluster0.yuiduwe.mongodb.net/conect-front-back?retryWrites=true&w=majority`;
+const uri_compass = `mongodb://localhost:${process.env.PORT_COMPASS}/test`;
+const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority`;
 const connectDb = async () => {
   try {
     await mongoose.connect(uri);
