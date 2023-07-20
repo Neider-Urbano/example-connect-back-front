65cc37ae61269a02a183c6a242a248a63c935e4c
Neider Urbano <jjuly8julianur@gmail.com>
2023-07-20 10:57:40 -0500

    constant uri compass and atlas

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 import "dotenv/config";
+const uri_atlas = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority`;
 const uri_compass = `mongodb://localhost:${process.env.PORT_COMPASS}/test`;
-const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.yuiduwe.mongodb.net/${process.env.NAME_DB_CLUSTER}?retryWrites=true&w=majority`;
+const uri = uri_atlas || uri_compass;
 const connectDb = async () => {
   try {
     await mongoose.connect(uri);
